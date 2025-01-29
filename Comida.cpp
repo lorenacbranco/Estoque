fe1791e0c439cbfb52a0fd3eb6053b3af0c1e03a
lorenacbranco <166522275+lorenacbranco@users.noreply.github.com>
2025-01-29 12:38:07 -0300

    Add files via upload

--- Comida.cpp
+++ Comida.cpp
@@ -1,45 +1,65 @@
 #include <iostream>
 #include <string>
-using namespace std;
-
+#include <vector>
 #include "Comida.h"
+
+using namespace std;
 
 // Construtor
 Comida::Comida(bool disponibilidade, double preco, string nome, int tempoPreparo, string tipo)
     : Produto(disponibilidade, preco, nome), tempoPreparo(tempoPreparo), tipo(tipo) {}
 
-//Exibe os detalhes das comidas
+    //Metodos
+
+// Exibe os detalhes das comidas
 void Comida::detalhes() const {
     Produto::detalhes();
-    cout<< "Tempo de Preparo:" << tempoPreparo << endl;
-    cout<< "Tipo............:" << tipo << endl;
+    cout << "Tempo de Preparo: " << tempoPreparo << endl;
+    cout << "Tipo............: " << tipo << endl;
 }
 
-//Define os preços de acordo com a classificação da comida
-void Comida::PrecoPorTipo(){
-    if (tipo == "Acompanhamentos"){
+// Define os preços de acordo com a classificação da comida
+void Comida::PrecoPorTipo() {
+    if (tipo == "Acompanhamentos") {
         setPreco(27.90);
     }
-    else if (tipo == "Blend MVK"){
+    else if (tipo == "Blend MVK") {
         setPreco(33.90);
     }
-    else if (tipo == "Hamburguer Double Smash"){
+    else if (tipo == "Hamburguer Double Smash") {
         setPreco(34.90);
     }
-    else if (tipo == "Hamburguer Smash"){
+    else if (tipo == "Hamburguer Smash") {
         setPreco(22.90);
     }
-    else if (tipo == "Blend Frango"){
+    else if (tipo == "Blend Frango") {
         setPreco(30.90);
     }
-    else if (tipo == "Sanduiches"){
+    else if (tipo == "Sanduiches") {
         setPreco(28.90);
     }
-    else if (tipo == "Cachorro Quente"){
+    else if (tipo == "Cachorro Quente") {
         setPreco(23.90);
     }
+}
 
+//Adiciona ingredientes
+void Comida::adicionarIngrediente(const Ingrediente& ingrediente) {
+    ingredientes.push_back(ingrediente);
+}
 
+//Subtrai a quantidade de ingredientes
+void Comida::fazerPedido() {
+    for (Ingrediente& ing : ingredientes) {
+        int quantidade = ing.getQuantidadeI();
+        if (quantidade > 0) {
+            // Subtrai uma unidade do ingrediente
+            ing.setQuantidadeI(quantidade - 1);
+            cout << "Ingrediente " << ing.getNomeI() << " agora tem " << ing.getQuantidadeI() << " unidades restantes." << endl;
+        } else {
+            cout << "Ingrediente " << ing.getNomeI() << " está fora de estoque." << endl;
+        }
+    }
 }
 
 // Getters e Setters
@@ -47,22 +67,19 @@ int Comida::getPreparo() const {
     return tempoPreparo;
 }
 
-void Comida::setPreparo(int novoPreparo){
+void Comida::setPreparo(int novoPreparo) {
     if (novoPreparo >= 0) {
         tempoPreparo = novoPreparo;
     } else {
-        cout << "Erro: tempo de preparo invalido!" << endl;
+        cout << "Erro: tempo de preparo inválido!" << endl;
     }
 }
 
-
-string Comida::getTipo() const{
+string Comida::getTipo() const {
     return tipo;
 }
 
-
-void Comida::setTipo(string novoTipo){
+void Comida::setTipo(string novoTipo) {
     tipo = novoTipo;
     PrecoPorTipo();
 }
-
--- Comida.h
+++ Comida.h
@@ -3,33 +3,31 @@
 
 #include <string>
 #include <iostream>
+#include <vector>  // Para o uso de lista de ingredientes
+#include "Produto.h"  //classe Produto
+#include "Ingrediente.h"  //classe Ingrediente
+
 using namespace std;
 
-#include "Produto.h" //Inclui a classe Produto
-
-class Comida : public Produto{ //Define como herança de Produto
+class Comida : public Produto {
 private:
-    int tempoPreparo; //Tempo médio de preparo em minutos
-    string tipo; //Tipo da comida (se é acompanhamento, se é hambúrguer smash...)
+    int tempoPreparo;  // Tempo medio de preparo em minutos
+    string tipo;  // Tipo da comida (se e acompanhamento, hamberguer, etc)
+    vector<Ingrediente> ingredientes;  // Lista de ingredientes (usando vector de Ingrediente)
 public:
+    Comida(bool disponibilidade, double preco, string nome, int tempoPreparo, string tipo); // Construtor
 
-    Comida(bool disponibilidade, double preco, string nome, int tempoPreparo, string tipo); //Construtor
-
-    //Métodos
-
+    // Metodos
     void detalhes() const override;
-    void PrecoPorTipo(); //Define os preços de acordo com a classificação da comida
-
+    void PrecoPorTipo(); // Define os precos de acordo com a classificacao da comida
+    void adicionarIngrediente(const Ingrediente& ingrediente);  //Adiciona ingredientes na comida
+    void fazerPedido();  //Subtrai a quantidade de ingredientes
 
     // Getters e Setters
     int getPreparo() const;
     void setPreparo(int novoPreparo);
-
     string getTipo() const;
     void setTipo(string novoTipo);
-
 };
-
-
 
 #endif // COMIDA_H_INCLUDED
