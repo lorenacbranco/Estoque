beed6f99c766bcf6ac760a23c695d5980a92dc2a
lorenacbranco <166522275+lorenacbranco@users.noreply.github.com>
2025-01-29 23:53:56 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,60 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <map>
 #include "Comida.h"
 #include "Ingrediente.h"
 #include "Bebida.h"
 
 using namespace std;
 
-int main(){
-    //Ingredientes
+int main() {
+    // Criando um mapa para armazenar ingredientes
+    map<string, Ingrediente> ingredientes = {
+        {"Blend de hamburguer", Ingrediente(10, "Blend de Hamburguer", "Carne", true)},
+        {"Mucarela", Ingrediente(12, "Mucarela", "Queijo", true)},
+        {"Tiras de bacon", Ingrediente(35, "Tiras de Bacon", "Carne", true)},
+        {"Alface", Ingrediente(10, "Alface", "Verdura", true)},
+        {"Tomate", Ingrediente(7, "Tomate", "Verdura", true)},
+        {"Cebola caramelizada", Ingrediente(10, "Cebola Caramelizada", "Verdura", true)},
+        {"Molho barbecue", Ingrediente(43, "Molho Barbecue", "Molho", true)},
+        {"Pao brioche", Ingrediente(10, "Pao Brioche", "Pao", true)}
+    };
 
-    //Verduras
-    Ingrediente alface(10, "Alface", "Verdura", true);
-    Ingrediente cebola(5, "Cebola", "Verdura", true);
-    Ingrediente cebolaCaramelizada(10, "Cebola Caramelizada", "Verdura", true);
-    Ingrediente cebolaRoxa(10, "Cebola Roxa", "Verdura", true);
-    Ingrediente cebolinha(10, "Cebolinha", "Verdura", true);
-    Ingrediente picles(10, "Picles", "Verdura", true);
-    Ingrediente tomate(7, "Tomate", "Verdura", true);
+    // Criando comidas e seus ingredientes
+    vector<Comida> comidas = {
+        Comida(true, 33.90, "Johnny Cash Burguer", 15, "Blend MVK",
+            {"Blend de hamburguer", "Mucarela", "Tiras de bacon", "Alface", "Tomate", "Cebola caramelizada", "Molho barbecue", "Pao brioche"})
+    };
 
-    //Carnes
-    Ingrediente blendFrangoEmp(8, "Blend de Frango Empanado", "Carne", true);
-    Ingrediente blendHamburguer(10, "Blend de Hamburger", "Carne", true);
-    Ingrediente blendVegano(10, "Blend Vegano", "Carne", true);
-    Ingrediente costelinha(20, "Costelinha Suina", "Carne", true);
-    Ingrediente salsicha(10, "Salsicha Viena", "Carne", true);
-    Ingrediente sobrecoxaFrango(5, "Sobrecoxa de Frango", "Carne", true);
-    Ingrediente tirasBacon(35, "Tiras de Bacon", "Carne", true);
-    Ingrediente tirasFrango(30, "Tiras de Peito de Frango", "Carne", true);
+    // Adicionando ingredientes as comidas
+    for (auto &comida : comidas) {
+        for (const string &nomeIngrediente : comida.getIngredientesNomes()) {
+            if (ingredientes.find(nomeIngrediente) != ingredientes.end()) {
+                comida.adicionarIngrediente(ingredientes[nomeIngrediente]);
+            }
+        }
+    }
 
-    //Acompanhamentos
-    Ingrediente batataFina(9, "Batata Fina", "Acompanhamento", true);
-    Ingrediente crispy(11, "Crispy de Cebola", "Acompanhamento", true);
-    Ingrediente dryreb(50, "Dry Reb", "Acompanhamento", true);
-    Ingrediente farofaBacon(25, "Farofa de Bacon", "Acompanhamento", true);
-    Ingrediente gergelim(9, "Gergelim", "Acompanhamento", true);
-    Ingrediente empanamento(11, "Ingredientes para Empanamento", "Acompanhamento", true);
-    Ingrediente pastel(25, "Massa de Pastel", "Acompanhamento", true);
+    // Criando bebidas
+    vector<Bebida> bebidas = {
+        Bebida(true, 4.50, "Agua", 350),
+        Bebida(true, 7.90, "Suco", 450),
+        Bebida(true, 7.90, "Coca-cola lata", 350),
+        Bebida(true, 13.90, "Milk-shake", 500)
+    };
 
-    //Molhos
-    Ingrediente ketchup(40, "Ketchup", "Molho", true);
-    Ingrediente ketchupGoiabada(32, "Ketchup Goiabada", "Molho", true);
-    Ingrediente maionese(40, "Maionese Temperada", "Molho", true);
-    Ingrediente island(22, "Molho 100 Island", "Molho", true);
-    Ingrediente barbecue(43, "Molho Barbecue", "Molho", true);
-    Ingrediente parmesao(49, "Molho Parmesão", "Molho", true);
+    // Exibindo comidas e bebidas
+    cout << "Cardapio de Comidas:\n";
+    for (const auto &comida : comidas) {
+        comida.detalhes();
+        cout << "----------------------\n";
+    }
 
-    //Queijos
-    Ingrediente creamCheese(6, "Cream Cheese", "Molho", true);
-    Ingrediente cheddar(24, "Cheddar Cremoso", "Molho", true);
-    Ingrediente mucarela(12, "Mucarela", "Molho", true);
-    Ingrediente provolone(6, "provolone", "Molho", true);
+    cout << "\nCardapio de Bebidas:\n";
+    for (const auto &bebida : bebidas) {
+        bebida.detalhes();
+        cout << "----------------------\n";
+    }
 
-    //Paes
-    Ingrediente american(10, "American Bread", "Pao", true);
-    Ingrediente brioche(10, "Brioche", "Pao", true);
-    Ingrediente paoGergelim(10, "Pao com Gergelim", "Pao", true);
-    Ingrediente hotdog(10, "Pao de Hot Dog", "Pao", true);
-    Ingrediente macio(10, "Macio", "Pao", true);
-
-    //Bebidas
-
-
+    return 0;
 }
